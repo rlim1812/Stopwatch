f18a359e043bc573cb4ec59799fde46aca8b3259
Ryan Lim <rlim1812@gmail.com>
2016-07-12 21:59:15 -0700

    First version of app

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var time = 0
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate() 
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        resultLabel.text = String(time)
+    }
+    
+    func increaseTimer(){
+        time++
+        resultLabel.text = String(time)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +43,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
